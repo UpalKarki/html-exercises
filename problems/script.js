c558c767a385aad15952e9274d5c026d67e83880
Upal Karki <karkiupal29@gmail.com>
2026-02-03 19:11:42 +0545

    Assignemnt

---
+++ problems/script.js
@@ -0,0 +1,187 @@
+// **Q: Write a for loop that prints numbers from 1 to 5 in the console.**
+// var num=1
+// if (num>0){
+//     console.log("Positive number")
+// }
+// else if (num<0){
+//     console.log("Negative number") 
+// }
+
+// **Q: Using a while loop, print even numbers from 2 to 10 (inclusive) in the console.**
+// let i=2
+// while (i<=10){
+//     console.log(i)
+//     i+=2
+// }
+
+// **Q: Write a for loop to calculate the sum of numbers from 1 to 10.**
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+// **Q: Using a while loop, find the factorial of a given number (e.g., 5).**
+// let n=5
+// let fact=1
+// while (n>0){
+//     fact=1*n;
+//     n--;
+//     console.log(fact)
+// }
+
+// **Q: Write a for loop that prints the square of numbers from 1 to 5.**
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i * i);
+//   i++;
+// }
+
+// **Q: Using a while loop, print the cube of numbers from 1 to 5.**
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i * i * i);
+//   i++;
+// }
+// }
+// **Q: Write a for loop to iterate through an array of names and print each name in the console.**
+// let name = ["Hari","Suraj","Dikshya","Upal"]
+// for(let i=0;i<name.length;i++){
+//     console.log(name[i])
+// }
+
+// **Q: Using a while loop, find the sum of elements in an array of numbers.**
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+// let i = 0;
+// while (i < numbers.length) {
+//   sum = sum + numbers[i];
+//   i++;
+// }
+// console.log(sum);
+// **Q: Write a for loop that prints the elements of an array in reverse order.**
+// let numbers = [1, 2, 3, 4, 5];
+// for(let i=numbers.length-1;i>=0;i--){
+//     console.log(numbers[i])
+// }
+
+// **Q: Using a while loop, find the largest number in an array of numbers.**
+// let numbers = [20, 35, 7, 40, 15];
+// let largest = numbers[0];
+// let i = 1;
+// while (i < numbers.length) {
+//   if (numbers[i] > largest) {
+//     largest = numbers[i];
+//   }
+//   i++;
+// }
+// console.log(largest);
+
+// **Q: Using a while loop, find the largest number in an array of numbers.**
+// const numbers = [25, 10, 45, 30, 15];
+// let largest = numbers[0];
+// let index = 1;
+// while (index &lt; numbers.length) {
+// if (numbers[index] &gt; largest) {
+// largest = numbers[index];
+// }
+// index++;
+// }
+// console.log(&quot;Largest Number:&quot;, largest);
+
+
+// **Q: Write a for loop to print the multiplication table of a given number (e.g.,
+// 5) up to 10.**
+// const num = 5;
+// for (let i = 1; i &lt;= 10; i++) {
+// console.log(num + &quot; x &quot; + i + &quot; = &quot; + num * i);
+// }
+
+// **Q: Using a while loop, find the largest element in an array of numbers.**
+// const numbers = [12, 43, 7, 25, 39];
+// let largest = numbers[0];
+// let index = 1;
+// while (index &lt; numbers.length) {
+// if (numbers[index] &gt; largest) {
+// largest = numbers[index];
+// }
+// index++;
+// }
+// console.log(&quot;Largest Number:&quot;, largest);
+
+// **Q: Write a for loop to print the following pattern:**
+// for (let i = 1; i &lt;= 5; i++) {
+// let row = &#39;&#39;;
+// for (let j = 1; j &lt;= i; j++) {
+// row += i;
+// }
+// console.log(row);
+// }
+
+// **Q: Using a while loop, find the number of digits in a given number (e.g.,
+// let number = 1234;
+// let count = 0;
+// while (number !== 0) {
+// number = Math.floor(number / 10);
+// count++;
+// }
+// console.log(&quot;Number of digits:&quot;, count);
+
+// **Q: Write a for loop to calculate the sum of all odd numbers from 1 to 50.**
+// let sum = 0;
+// for (let i = 1; i &lt;= 50; i += 2) {
+// sum += i;
+// }
+// console.log(&quot;Sum of odd numbers:&quot;, sum);
+
+
+// **Q: Using a while loop, print the numbers from 1 to 100, but skip the
+// multiples of 5.**
+// let num = 1;
+// while (num &lt;= 100) {
+// if (num % 5 !== 0) {
+// console.log(num);
+// }
+// num++;
+// }
+
+// **Q: Write a for loop to print the reverse of a given string (e.g., &quot;hello&quot;).**
+// const str = &quot;hello&quot;;
+// let reversedStr = &quot;&quot;;
+// for (let i = str.length - 1; i &gt;= 0; i--) {
+// reversedStr += str[i];
+// }
+// console.log(&quot;Reversed string:&quot;, reversedStr);
+
+// **Q: Using a while loop, find the factorial of a given number (e.g., 7).**
+// let num = 7;
+// let factorial = 1;
+// while (num &gt; 0) {
+// factorial *= num;
+// num--;
+// }
+// console.log(&quot;Factorial:&quot;, factorial);
+
+// **Q: Write a for loop to print the following pattern:**
+// for (let i = 1; i &lt;= 5; i++) {
+// let row = &#39;&#39;;
+// for (let j = 1; j &lt;= i; j++) {
+// row += j;
+// }
+// console.log(row);
+// }
+
+// **Q: Using a while loop, find the sum of all elements in a multidimensional
+// array of numbers.**
+// const numbers = [[1, 2], [3, 4], [5, 6]];
+// let sum = 0;
+// let i = 0;
+// while (i &lt; numbers.length) {
+// let j = 0;
+// while (j &lt; numbers[i].length) {
+// sum += numbers[i][j];
+// j++;
+// }
+// i++;
+// }
+// console.log(&quot;Sum:&quot;, sum);
+
